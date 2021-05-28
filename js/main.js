395f3fd7f358ebad8308e1496e6c6261f4029e5b
sadirov4647 <sadirovizzatillo3@gmail.com>
2021-05-28 11:47:07 +0500

    burger menu rasponsive done

---
+++ js/main.js
@@ -0,0 +1,45 @@
+var element = {
+    select: document.querySelector(".burger-btn"),
+    active: "active",
+    className: function () {
+        return this.select.className;
+    }
+};
+
+element.select.onclick = function () {
+    var checker = hasClass(element.select, element.active);
+
+    if (checker) {
+        removeClass(element.select, element.active);
+    } else {
+        element.select.className = element.select.className + " active";
+    }
+};
+
+function hasClass(elem, className) {
+    return new RegExp(" " + className + " ").test(" " + elem.className + " ");
+}
+
+function removeClass(elem, className) {
+    var newClass = " " + elem.className.replace(/[\t\r\n]/g, " ") + " ";
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0) {
+            newClass = newClass.replace(" " + className + " ", " ");
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, "");
+    }
+}
+
+
+document.querySelector('.openMenu').addEventListener('click', () => {
+    document.querySelector('.nav-info__list').classList.toggle('open');
+    document.querySelector('.nav-page__list').classList.toggle('open');
+    document.querySelector('.choose-section__list').classList.toggle('active');
+});
+
+document.querySelector('.search-btn').addEventListener('click', () => {
+    document.querySelector('.search-input').classList.toggle('input-block');
+    document.querySelector('.search-btn').style.display = "none";
+})
+
+
